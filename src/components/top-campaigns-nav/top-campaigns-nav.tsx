65ce3f93b6d26d43b82d0f4205358951a2a9eab8
Maya <mayaw10@gmail.com>
2021-02-22 20:07:51 +0200

    First font face on index.css

--- src/components/top-campaigns-nav/top-campaigns-nav.tsx
+++ src/components/top-campaigns-nav/top-campaigns-nav.tsx
@@ -200,54 +200,6 @@ export class TopCampaignsNav extends Component<TopCampaignsNavProps, TopCampaign
                         </button>
                     )}
 
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
-<button className="campaigns-client-btn" >
-                            <button className="campaigns-remove-btn">
-                                <span>&#10006;</span>
-                            </button>
-                            <span className="campaigns-inside-client-btn">ffff</span>
-                        </button>
 
 
 
